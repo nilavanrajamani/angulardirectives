8524c61a2bada3ae66a7b097452be3f7918e5bee
Anbumozhi Rajamani <nilavan.anbumozhirajamani@cgi.com>
2017-04-21 15:40:00 +0530

    ''

--- script.js
+++ script.js
@@ -2,5 +2,12 @@
 var app = angular.module('app',[]);
 
 app.controller('mainCtrl', function($scope){
-    
+
+});
+
+app.directive('userInfoCard', function(){
+    return {
+        restrict: 'E',  
+        template: "User Info Card"
+    }
 });
